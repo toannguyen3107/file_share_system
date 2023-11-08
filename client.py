8903b68d1e8a22f1cbfb45e2d99e4518ff4a9c5d
Toan Nguyen <toan.nguyenminh@hcmut.edu.vn>
2023-11-08 23:29:23 +0700

    update concurrent for server - update publish for client

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import shutil
 file_map = {}
 
 sep = '<sep>'
-
+repdir = '/repository'
 class Client:
 
     def __init__(self, command):
@@ -14,45 +14,44 @@ class Client:
         self.arrCommand = command.strip().split(' ')
     def checkvalidcommand(self):
         if self.arrCommand[0] == 'publish' and len(self.arrCommand) != 3:
-            print('Command invalid')
+            print('Command invalid -0')
         elif len(self.arrCommand) != 2:
-            print('Command invalid')
-    def handlerFile(self):
+            print('Command invalid -1')
+    def handlerFile(self, dircheck):
         lname = self.arrCommand[1]
         fname = self.arrCommand[2]
-        currFname = './repository'+ fname
-        shutil(lname, currFname)
-
+        shutil.copyfile(lname, os.path.join(dircheck, fname))
+    
     def publish(self, host, port):
         # get lname and fname
         lname = self.arrCommand[1]
         fname = self.arrCommand[2]
 
-        pathcheck = './repository/'+ lname
-        if not os.path.exists(pathcheck):
+        # check dir path
+        dircheck =os.path.join(os.getcwd(), 'repository')
+        if not os.path.exists(dircheck):
+            os.mkdir('repository')
+        elif not os.path.exists(lname):
             print('the lname is a invalid path')
-            sys.exit(1)
-        self.handlerFile()
-    # check fname valid before sending to the server!
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sendSeg = 'publish' + sep + fname # merge sender
-            sock.connect((host, port))
-            sock.send(sendSeg.encode())
-            
-            status = sock.recv(1024).decode()
-            if status == 'upload fname success!': 
-                file_map[fname] = './repository' + lname
-                print(file_map)
-
+        else:
+        # check fname valid before sending to the server!
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sendSeg = 'publish' + sep + fname # merge sender
+                sock.connect((host, port))
+                sock.send(sendSeg.encode())
+                
+                status = sock.recv(1024).decode()
+                if status == '200': 
+                    self.handlerFile(dircheck)
+                    print('success - fetch!')
     def fetch(self, host, port):
         pass
 if __name__ == '__main__':
     host = '192.168.0.100'
     port = 12345
-
     print('[+] This is system for sharing file:')
     print('\t[-] Command list:')
-    print('\t. public <lname> <fname>')
+    print('\t. publich <lname> <fname>')
     print('\t. fetch <fname>')
     print('\t. out')
     while True:
@@ -67,6 +66,6 @@ if __name__ == '__main__':
         elif command.strip() == 'out':
             sys.exit(1)
         else:
-            print('Command invalid')
+            print('Command invalid -3')
 
     print('have problem')
--- server.py
+++ server.py
@@ -2,13 +2,14 @@ import socket
 import threading
 import multiprocessing
 import concurrent.futures
-
+import queue
 sep = '<sep>'
 
-file_map = {} # ele follow form: {fname: (addr, port)}
+file_map = {}  # ele follow form: {fname: (addr, port)}
+
 
 class MyServer:
-    
+
     def __init__(self, host, port, max_threads):
         self.host = host
         self.port = port
@@ -19,7 +20,8 @@ class MyServer:
             sock.bind((self.host, self.port))
             sock.listen(5)
 
-            accept_thread = threading.Thread(target=self.accept_connections, args=(sock,))
+            accept_thread = threading.Thread(
+                target=self.accept_connections, args=(sock,))
             accept_thread.start()
             accept_thread.join()
 
@@ -27,40 +29,63 @@ class MyServer:
         # Your code to handle the connection goes here
         sendSeg = conn.recv(1024).decode()
         arrSend = sendSeg.strip().split(sep)
-        
+
         if arrSend[0] == 'publish':
             file_map[arrSend[1]] = addr[0]
+            print(f"--{addr}--")
+            conn.send('200'.encode())
         elif arrSend[0] == 'fetch':
             pass
+
     def accept_connections(self, sock):
         # Threadpool - using though module!
         with concurrent.futures.ThreadPoolExecutor(self.max_threads) as executor:
             while True:
                 conn, addr = sock.accept()
                 executor.submit(self.handle_connection, conn, addr)
-def commandProcess():
-    print('[+] This server-side of file transfer system')
+
+
+command_queue = queue.Queue()
+
+
+def commandProcess(command_queue):
+    print('[+] This is the server-side of the file transfer system')
     print('\t[-] Command List:')
     print('\t. discover <hostname>')
     print('\t. ping <hostname>')
-    command = input('\t[-] Type command: ')
-    if command.startswith('discover'):
-        print('discover command!')
+    print('\t. out')
+
+    while True:
+        try:
+            # Check if there are commands in the queue
+            command = command_queue.get_nowait()
+            print(f"[-] Received command: {command}")
+            if command.startswith('discover'):
+                print('discover command!')
+        except queue.Empty:
+            # No command in the queue, you can do other processing here if needed
+            pass
+
+
 if __name__ == "__main__":
     host = '0.0.0.0'
     port = 12345
     max_threads = 10
 
-    p1 = multiprocessing.Process(target=commandProcess())
-
+    p1 = multiprocessing.Process(target=commandProcess, args=(command_queue,))
     server = MyServer(host, port, max_threads)
-    p2 = multiprocessing.Process(target=server.start())
-    
-    p1.start()
-    p2.start()
+    p2 = multiprocessing.Process(target=server.start)
 
+    p2.start()
+    p1.start()
+
+    while True:
+        # Allow the user to input commands and put them in the queue
+        user_input = input('[-] Type a command (or "out" to exit): ')
+        command_queue.put(user_input)
+
+        if user_input == 'out':
+            break
 
     p1.join()
     p2.join()
-
-
